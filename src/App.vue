b17d5e156b0fc63725686635b8b94fd1fc74a13f
Roel Castro <rcastro_5450@stu.southtexascollege.edu>
2025-02-09 17:04:27 -0600

    Initial commit of all files

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      ingredient: '',
+      direction: '',
+      ingredients: [],
+      directions: []
+    };
+  },
+  methods: {
+    addIngredient() {
+      if (this.ingredient) {
+        this.ingredients.push(this.ingredient);
+        this.ingredient = '';  // Clear the input field
+      }
+    },
+    addDirection() {
+      if (this.direction) {
+        this.directions.push(this.direction);
+        this.direction = '';  // Clear the input field
+      }
+    }
+  }
+};
+</script>
+<template>
+  <div>
+    <h2>Add Ingredients</h2>
+    <input v-model="ingredient" placeholder="Enter ingredient" />
+    <button @click="addIngredient">Add Ingredient</button>
+    <ul>
+      <li v-for="(item, index) in ingredients" :key="index">{{ item }}</li>
+    </ul>
+
+    <h2>Add Directions</h2>
+    <input v-model="direction" placeholder="Enter direction" />
+    <button @click="addDirection">Add Direction</button>
+    <ul>
+      <li v-for="(item, index) in directions" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
